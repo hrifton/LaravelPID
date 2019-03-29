68831f9169bdff31f5ac16a66db6f9a9f39e8d67
Oussama Husain <oussama.husain@gmail.com>
2019-03-29 23:18:19 +0100

    test du push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Test Laravel
                 </div>
 
                 <div class="links">
