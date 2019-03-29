50700ba004dbbeedabf65bd8e431ecc0ced2a7cf
hrifton <asbagui.hicham@outlook.com>
2019-03-29 22:30:11 +0100

    test depuis le terminal

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        Exemple Vues .
+                        Exemple Vue .
                     </div>
                 </div>
             </div>
